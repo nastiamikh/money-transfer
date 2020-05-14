75e4601afd3186bae5366ca23a1fba20bed146a6
Nastia Mikh <nastiamikh@mail.ru>
2020-05-15 01:57:34 +0800

    Test report added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int deposit = 2_000_000_000;
-        int transfer =500_000_000;
+        int transfer =147483648;
         int balance = deposit + transfer;
         System.out.println(balance);
     }
